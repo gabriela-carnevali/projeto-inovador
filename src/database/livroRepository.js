3d3187d1dea34e7641c187353bf6f933accf70a0
gabriela-carnevali <gabriela.c.lima10@aluno.senai.br>
2026-08-26 15:09:32 -0300

    Programação da database e livroCard

---
+++ src/database/livroRepository.js
@@ -0,0 +1,16 @@
+import { db } from './database';
+
+export const livroRepository = {
+  // READ: Busca os livros de acordo com a aba/tela
+  buscarPorStatus: (status) => {
+    return db.getAllSync('SELECT * FROM livros WHERE status = ?;', [status]);
+  },
+
+  // CREATE: Insere um novo livro cadastrado
+  adicionar: (titulo, autor, status) => {
+    return db.runSync(
+      'INSERT INTO livros (titulo, autor, status, progresso) VALUES (?, ?, ?, 0);',
+      [titulo, autor, status]
+    );
+  }
+};
